fd957c933523301196e8edddfbc1c8ad69222929
Darina Samokhina <mokhinsam@yandex.ru>
2024-04-29 16:00:32 +0300

    Added NavigationController and WeatherViewController in Main. Added all interface elements to the screen WeatherViewController, created a class for WeatherViewController.

---
+++ WeatherApp/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Дарина Самохина on 25.04.2024.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet var currentPlaceLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var weatherImage: UIImageView!
+    @IBOutlet var minMaxLabel: UILabel!
+    @IBOutlet var weatherForecastTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+}
+
